1cd8a55d060a5cc0966dbff37832996c69e6fcfe
abhijeet-1001 <Abhi@2022@>
2022-05-10 19:41:21 +0530

    Initial commit

---
+++ src/Abhi.java
@@ -0,0 +1,19 @@
+
+
+    public class Abhi
+    {
+        public static final int a;
+
+        static{
+            a = 10;
+            System.out.println(a);
+        }
+
+        public static void main(String args[])
+        {
+            System.out.println("Hello, I am Abhijeet !");
+
+        }
+
+    }
+
